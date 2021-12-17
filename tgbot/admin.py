d1d5fc001ae4956a561a55b7329c8a18200c8baf
Valery Efremov <valeriy131100@gmail.com>
2021-12-17 10:17:58 +0300

    Add make_recipients as admin action

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Game, User, GameParticipant
 
 
-admin.site.register(Game)
+@admin.action(description='Устроить жеребьевку участников')
+def make_recipients_in_game(modeladmin, request, queryset):
+    for game in queryset:
+        game.calculate_recipients_in_game()
+
+
+class GameAdmin(admin.ModelAdmin):
+    actions = [make_recipients_in_game]
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(User)
 admin.site.register(GameParticipant)
