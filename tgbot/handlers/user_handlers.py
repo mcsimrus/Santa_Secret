f231891f5718b59f6f87af5edfd6c5ed533c6682
Valery Efremov <valeriy131100@gmail.com>
2021-12-17 10:53:29 +0300

    Save user info to database

--- tgbot/handlers/user_handlers.py
+++ tgbot/handlers/user_handlers.py
@@ -1,14 +1,27 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CallbackContext, ConversationHandler
+from tgbot.models import User, GameParticipant, Game
 
 GET_NAME, GET_EMAIL, GET_WISH_LIST, GET_SANTA_LETTER = range(10, 14)
 
 
 def get_name(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Введите свое имя:'
-    )
-    return GET_NAME
+    try:
+        user = User.objects.get(telegram_id=update.message.from_user['id'])
+    except User.DoesNotExist:
+        update.message.reply_text(
+            'Введите свое имя:',
+            reply_markup=ReplyKeyboardRemove()
+        )
+        return GET_NAME
+    else:
+        update.message.reply_text(
+            'Вижу вы участвуете в наших играх не в первый раз. Можем сразу переходить к списку пожеланий.'
+        )
+        context.user_data['model_object'] = user
+        context.user_data['name'] = user.fio
+        context.user_data['email'] = user.email
+        return get_wish_list(update, context)
 
 
 def get_email(update: Update, context: CallbackContext):
@@ -21,7 +34,8 @@ def get_email(update: Update, context: CallbackContext):
 
 
 def get_wish_list(update: Update, context: CallbackContext):
-    context.user_data['email'] = update.message.text
+    if not context.user_data.get('model_object'):
+        context.user_data['email'] = update.message.text
 
     keyboard = [
         [
@@ -45,7 +59,7 @@ def get_santa_letter(update: Update, context: CallbackContext):
     if update.message.text != 'Пропустить':
         context.user_data['wish_list'] = update.message.text
     else:
-        context.user_data['wish_list'] = None
+        context.user_data['wish_list'] = ''
 
     keyboard = [
         [
@@ -69,7 +83,7 @@ def end_registration(update: Update, context: CallbackContext):
     if update.message.text != 'Пропустить':
         context.user_data['santa_letter'] = update.message.text
     else:
-        context.user_data['santa_letter'] = None
+        context.user_data['santa_letter'] = ''
 
     update.message.reply_text(
         'Превосходно, ты в игре! 31.12.2021 мы проведем жеребьёвку '
@@ -87,7 +101,23 @@ def end_registration(update: Update, context: CallbackContext):
 
     update.message.reply_text(user_data_message)
 
-    # here be database things
+    user_id = update.message.from_user['id']
+    user_model_object = context.user_data.get('model_object')
+
+    if not user_model_object:
+        User.objects.create(
+            telegram_id=user_id,
+            fio=user_data['name'],
+            email=user_data['email']
+        )
+        user_model_object = User.objects.get(telegram_id=update.message.from_user['id'])
+
+    GameParticipant.objects.create(
+        user=user_model_object,
+        game=Game.objects.get(id=user_data['game_id']),
+        wish_list=user_data['wish_list'],
+        santa_letter=user_data['santa_letter']
+    )
 
     return ConversationHandler.END
 
